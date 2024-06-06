bfb5d7dcfb511326e55cb138eb6f8e84d1e969f8
Nate Vaughan <nate.vaughan@protonmail.com>
2024-06-06 09:41:36 -0500

    Playing with env vars

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    myvar: String,
+}
+
 fn main() {
-    invalid syntax
-    println!("the rust code compiled, built, and ran!!!!");
+    let args = Cli::parse();
+    println!(
+        "the rust code compiled, built, and ran with myvar={}",
+        args.myvar
+    );
 }
