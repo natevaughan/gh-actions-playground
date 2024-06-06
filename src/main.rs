8ac37d2e90638ca7fb750ac101b2ea8b65e511e9
Nate Vaughan <nate.vaughan@protonmail.com>
2024-06-06 09:50:43 -0500

    Pull env vars into workflow env

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(short, long)]
-    myvar: String,
+    myvar: Option<String>,
+
+    #[arg(short, long)]
+    secret: Option<String>,
 }
+
+static NONE: &str = "<none>";
 
 fn main() {
     let args = Cli::parse();
     println!(
-        "the rust code compiled, built, and ran with myvar={}",
-        args.myvar
+        "the rust code compiled, built, and ran with myvar={} and mysecret={}",
+        args.myvar.unwrap_or(NONE.to_owned()),
+        args.secret.unwrap_or(NONE.to_owned())
     );
 }
